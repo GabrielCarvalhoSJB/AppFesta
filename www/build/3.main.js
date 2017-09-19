feda83bd8cc9c048c4f50aecd61de0abe96c7bc5
gabrielcarvalho1 <gcarvalho915@gmail.com>
2017-09-19 11:01:41 -0300

    'teste'

--- www/build/3.main.js
+++ www/build/3.main.js
@@ -1,14 +1,14 @@
 webpackJsonp([3],{
 
-/***/ 323:
+/***/ 326:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(25);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__bares__ = __webpack_require__(328);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "BaresPageModule", function() { return BaresPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(14);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__buteco_du__ = __webpack_require__(54);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ButecoDuPageModule", function() { return ButecoDuPageModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -18,23 +18,23 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
-var BaresPageModule = (function () {
-    function BaresPageModule() {
+var ButecoDuPageModule = (function () {
+    function ButecoDuPageModule() {
     }
-    return BaresPageModule;
+    return ButecoDuPageModule;
 }());
-BaresPageModule = __decorate([
+ButecoDuPageModule = __decorate([
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_2__bares__["a" /* BaresPage */],
+            __WEBPACK_IMPORTED_MODULE_2__buteco_du__["a" /* ButecoDuPage */],
         ],
         imports: [
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__bares__["a" /* BaresPage */]),
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__buteco_du__["a" /* ButecoDuPage */]),
         ],
     })
-], BaresPageModule);
+], ButecoDuPageModule);
 
-//# sourceMappingURL=bares.module.js.map
+//# sourceMappingURL=buteco-du.module.js.map
 
 /***/ })
 
--- www/build/4.main.js
+++ www/build/4.main.js
@@ -6,9 +6,9 @@ webpackJsonp([4],{
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(25);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__cooka_restobar__ = __webpack_require__(327);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CookaRestobarPageModule", function() { return CookaRestobarPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(14);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__bares__ = __webpack_require__(110);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "BaresPageModule", function() { return BaresPageModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -18,23 +18,23 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
-var CookaRestobarPageModule = (function () {
-    function CookaRestobarPageModule() {
+var BaresPageModule = (function () {
+    function BaresPageModule() {
     }
-    return CookaRestobarPageModule;
+    return BaresPageModule;
 }());
-CookaRestobarPageModule = __decorate([
+BaresPageModule = __decorate([
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_2__cooka_restobar__["a" /* CookaRestobarPage */],
+            __WEBPACK_IMPORTED_MODULE_2__bares__["a" /* BaresPage */],
         ],
         imports: [
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__cooka_restobar__["a" /* CookaRestobarPage */]),
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__bares__["a" /* BaresPage */]),
         ],
     })
-], CookaRestobarPageModule);
+], BaresPageModule);
 
-//# sourceMappingURL=cooka-restobar.module.js.map
+//# sourceMappingURL=bares.module.js.map
 
 /***/ })
 
